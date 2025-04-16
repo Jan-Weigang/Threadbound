875666589f9cf7311d8b6feb6b6289969da4526e
Jan-Weigang <jan.webservice@weigang-music.de>
2025-04-16 18:45:44 +0200

    Changed attendees needed for deletion to >1 to account for default behavior of form with attending creator

--- tt_calendar/utils.py
+++ tt_calendar/utils.py
@@ -182,7 +182,7 @@ def is_event_deletable(event: Event) -> bool:
     one_month_from_now = now + timedelta(days=30)
 
     # Check if the event is within one month and has attendees
-    if event.start_time <= one_month_from_now or len(event.attendees) > 0: # type: ignore
+    if event.start_time <= one_month_from_now or len(event.attendees) > 1: # type: ignore
         return False
     return True
 
